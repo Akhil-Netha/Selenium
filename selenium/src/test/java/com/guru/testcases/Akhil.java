6014104d4bad2c1aa5c7f78735a716e2cf2ab715
uday_2 <uday_2@VAIO>
2021-08-01 18:39:28 +0530

    Akhil : Akhil Test Case Updated

---
+++ selenium/src/test/java/com/guru/testcases/Akhil.java
@@ -0,0 +1,14 @@
+package com.guru.testcases;
+
+public class Akhil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		System.out.println("Akhi");
+	}
+
+}
